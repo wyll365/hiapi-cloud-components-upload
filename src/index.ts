99009e6e0603eb3bd18a81c003baccc85b702cd4
adinz <adinz@adinz.com>
2025-11-13 18:35:28 +0800

    Init

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ export interface UploadPluginOptions {
     appId?: string; // 可选鉴权 token
     appSecret?: string; // 可选鉴权 token
     file: string; // 默认 dist/index.umd.js
+    disable?: boolean; //是否禁用上传
+    version?: string; //插件版本
 }
 interface UploadResponse<T> {
     code :number
@@ -18,12 +20,17 @@ interface UploadResponse<T> {
 
 
 export function UploadPlugin(options: UploadPluginOptions): Plugin {
-    const {server, appId, appSecret, file } = options
+    const {server, appId, appSecret, file,version='0.0.0',disable=false } = options
 
     return {
         name: 'vite-upload-plugin',
         apply: 'build',
         async closeBundle() {
+
+            if (disable===true){
+                return //上传插件被禁用,
+            }
+
             console.log(`\n📦  开始上传编译文件到服务器: ${server}`)
 
             const distPath = path.resolve(process.cwd(), file)
@@ -36,6 +43,7 @@ export function UploadPlugin(options: UploadPluginOptions): Plugin {
                 const form = new FormData()
                 form.append('appId', appId)
                 form.append('appSecret', appSecret)
+                form.append('version', version)
                 form.append('file', fs.createReadStream(distPath))
 
                 const response: AxiosResponse<UploadResponse<any>> = await axios.post(server, form, {
