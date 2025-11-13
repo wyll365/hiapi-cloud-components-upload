1f6afe8b24ccb6e958b32e22fc42abb40c71db58
adinz <adinz@adinz.com>
2025-11-13 17:23:08 +0800

    Init

---
+++ README.md
@@ -0,0 +1,12 @@
+# HIAPI-Cloud 项目 vite插件
+
+仅用于 HIAPI-Cloud uniapp前端项目
+
+此Vite插件 适用于 HiAPI-Cloud 子应用前端开发
+
+
+发布: `npm publish --access public`
+
+npm config set registry https://registry.npmjs.org/
+
+npm config set registry https://registry.npmmirror.com/
---
+++ src/index.ts
@@ -0,0 +1,57 @@
+import type { Plugin } from 'vite'
+import fs from 'fs'
+import path from 'path'
+import axios, {type AxiosResponse} from 'axios'
+import FormData from 'form-data'
+
+export interface UploadPluginOptions {
+    server: string; // 服务器上传地址，例如 https://api.example.com/upload
+    appId?: string; // 可选鉴权 token
+    appSecret?: string; // 可选鉴权 token
+    file: string; // 默认 dist/index.umd.js
+}
+interface UploadResponse<T> {
+    code :number
+    message : string
+    data : T
+}
+
+
+export function UploadPlugin(options: UploadPluginOptions): Plugin {
+    const {server, appId, appSecret, file } = options
+
+    return {
+        name: 'vite-upload-plugin',
+        apply: 'build',
+        async closeBundle() {
+            console.log(`\n📦  开始上传编译文件到服务器: ${server}`)
+
+            const distPath = path.resolve(process.cwd(), file)
+
+            if (!fs.existsSync(distPath)) {
+                console.error(`❌ 文件不存在: ${distPath}`)
+                return
+            }
+            try {
+                const form = new FormData()
+                form.append('appId', appId)
+                form.append('appSecret', appSecret)
+                form.append('file', fs.createReadStream(distPath))
+
+                const response: AxiosResponse<UploadResponse<any>> = await axios.post(server, form, {
+                    headers: {
+                        ...form.getHeaders(),
+                    },
+                    maxBodyLength: Infinity,
+                })
+                if (response.data.code !== 200) {
+                    console.error(`❌ 服务器返回失败: ${response.data.message}`)
+                }
+                console.log(`✅ 已上传: ${path.relative(distPath, file)}`)
+            } catch (err: any) {
+                console.error(`❌ 上传失败: ${file}`)
+                console.error(err.message)
+            }
+        },
+    }
+}
